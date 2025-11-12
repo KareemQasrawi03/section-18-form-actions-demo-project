b6ca9189b0d288557680aef514f19accf4388212
Kareem Qasrawi <kareemalqasrawi@gmail.com>
2025-11-12 16:14:41 +0300

    add useOptimistic this is very nice hook

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,14 +1,19 @@
 import { use } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 import { useActionState } from "react";
+import { useOptimistic } from "react";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
 
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(votes,(prevVotes,mode)=> mode === 'up'? prevVotes + 1 : prevVotes - 1)
+
   async function upvote() {
+    setVotesOptimistically("up")
     await upvoteOpinion(id);
   }
   async function downvote() {
+    setVotesOptimistically("down")
     await downvoteOpinion(id);
   }
 
@@ -46,7 +51,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button
           formAction={downvoteFormAction}
